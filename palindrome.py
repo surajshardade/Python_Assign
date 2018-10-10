5b121f9c27893a71f493e699d52c5d1957b10904
surajshardade <surajshardade@gmail.com>
2018-10-10 15:19:36 +0530

    palindrome string

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+
+
+def palindrome(str1):
+    last=len(str1)-1
+    for index in range(int(len(str1)/2)):
+        if str1[index]!=str1[last-index]:
+            return False
+        return True
+            
+
+
+if __name__=="__main__":
+    str1=input("Enter string : ")
+    res=palindrome(str1)
+    print("Palindrome : {}".format(res))
